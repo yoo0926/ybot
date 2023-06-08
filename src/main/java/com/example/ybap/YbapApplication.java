d22ae5608edd9edbfa2def7df24f5e5dd5df9079
유재용 <wodydfasktp@ybtour.co.kr>
2023-06-08 16:49:45 +0900

    init project

---
+++ src/main/java/com/example/ybap/YbapApplication.java
@@ -0,0 +1,13 @@
+package com.example.ybap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YbapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YbapApplication.class, args);
+	}
+
+}
